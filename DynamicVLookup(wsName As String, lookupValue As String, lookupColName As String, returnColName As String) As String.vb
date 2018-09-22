76fbd468090ec44dcca7fb1e074738835a4f8f22
Quinn McHugh <qpmchugh@outlook.com>
2018-09-21 23:34:14 -0400

    Update DynamicVLookup(wsName As String, lookupValue As String, lookupColName As String, returnColName As String) As String.vb

--- DynamicVLookup(wsName As String, lookupValue As String, lookupColName As String, returnColName As String) As String.vb
+++ DynamicVLookup(wsName As String, lookupValue As String, lookupColName As String, returnColName As String) As String.vb
@@ -2,12 +2,12 @@
 '' Program:   DynamicVLookup
 '' Desc:      Looks up a user-specified value in a user-specified column in a user-specified
 ''            worksheet and returns another value that is in the same row but is in a different
-''            user-specified column
+''            user-specified column.
 '' Calls:     getColumnNumber, colNum2Letter
-'' Arguments: wsName        -- The name of the worksheet where the lookup should be performed
-''            lookupValue   -- The value to be looked up
-''            lookupColName -- The name of the column that contains the lookup value
-''            returnColName -- the name of the column that contains the desired return value
+'' Arguments: wsName        -- The name of the worksheet where the lookup should be performed.
+''            lookupValue   -- The value to be looked up.
+''            lookupColName -- The name of the column that contains the lookup value.
+''            returnColName -- the name of the column that contains the desired return value.
 '' Comments:  This function should only be used in spreadsheets where all column titles are on
 ''            the first row and all data is listed underneath. Like so:
 ''            First Name | Last Name | Favorite Color
